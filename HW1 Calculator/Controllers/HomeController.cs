aacce7f05fd4bbab5e217bacfbecd640ffa3e412
noahanderson3 <noahanderson57@gmail.com>
2022-03-20 14:38:00 -0400

    Initial Upload

---
+++ HW1 Calculator/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using HW1_Calculator.Models;
+
+namespace HW1_Calculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.Result = 0;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Calculator calc, string type)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Result = calc.Calculate(type);
+            }
+            else
+                ViewBag.Result = 0;
+
+            return View(calc);
+        }
+    }
+}
---
+++ HW1 Calculator/Models/Calculator.cs
@@ -0,0 +1,29 @@
+﻿namespace HW1_Calculator.Models
+{
+    public class Calculator
+    {
+        public decimal firstInput { get; set; }
+        public decimal secondInput { get; set; }
+        public decimal result { get; set; }
+
+        public decimal? Calculate(string type)
+        {
+            switch (type)
+            {
+                case "add":
+                    result = firstInput + secondInput;
+                    break;
+                case "minus":
+                    result = firstInput - secondInput;
+                    break;
+                case "divide":
+                    result = firstInput / secondInput;
+                    break;
+                case "multiply":
+                    result = firstInput * secondInput;
+                    break;
+            }
+                return result;
+        }
+    }
+}
